ce5578168a66997a6063698c35b459611e56fec9
Seth Lachman <lachmanseth17@gmail.com>
2019-05-20 13:39:03 -0400

    Created components folder and added react router with first route.

--- macros/src/App.js
+++ macros/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home';
 import './App.css';
 
 function App() {
 	return (
-		<div>
-			<h1>Macros App</h1>
-		</div>
+		<Router>
+			<Route exact path="/" component={Home} />
+		</Router>
 	);
 }
 
