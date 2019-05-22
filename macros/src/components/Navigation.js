e5db7cef8ae0e7bdc2a926691a9956610de263a7
Seth Lachman <lachmanseth17@gmail.com>
2019-05-22 13:56:29 -0400

    Added styles to navigation area.

--- macros/src/components/Navigation.js
+++ macros/src/components/Navigation.js
@@ -1,27 +1,54 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import styled from "styled-components";
+
+const NavBar = styled.nav`
+  background: #333;
+`;
+
+const NavContainer = styled.ul`
+  margin: 0;
+  list-style: none;
+  padding: 1.4rem 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const NavItem = styled.li`
+  display: inline-block;
+  margin: 0 1.3rem;
+`;
+
+const NavElement = styled(NavLink)`
+  font-size: 1.5rem;
+  color: #f9f9f9;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up">Sign Up</NavLink>
-        </li>
-        <li>
-          <NavLink to="/meal-page">Meal Page</NavLink>
-        </li>
-        <li>
-          <NavLink to="/update-user">Update Weight/Macro</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <NavBar>
+      <NavContainer>
+        <NavItem>
+          <NavElement to="/">Home</NavElement>
+        </NavItem>
+        <NavItem>
+          <NavElement to="/login">Login</NavElement>
+        </NavItem>
+        <NavItem>
+          <NavElement to="/sign-up">Sign Up</NavElement>
+        </NavItem>
+        <NavItem>
+          <NavElement to="/meal-page">Meal Page</NavElement>
+        </NavItem>
+        <NavItem>
+          <NavElement to="/update-user">Update Weight/Macro</NavElement>
+        </NavItem>
+      </NavContainer>
+    </NavBar>
   );
 };
 
