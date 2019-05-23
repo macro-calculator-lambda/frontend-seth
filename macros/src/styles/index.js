8a2daf60ef780d6508134b173fbc4a9c3f656dbc
Seth Lachman <lachmanseth17@gmail.com>
2019-05-23 17:33:00 -0400

    Organizational clean up.

--- macros/src/styles/index.js
+++ macros/src/styles/index.js
@@ -1,4 +1,32 @@
-import styled, { css } from "styled-components";
+import styled, { css, createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    background: #FCF9FB;
+    font-family: 'Muli', sans-serif;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  p {
+    font-size: 1.6rem;
+  }
+
+  li {
+    font-size: 1.5rem;
+  }
+`;
 
 export const Container = styled.div`
   margin: 4rem auto 0;
